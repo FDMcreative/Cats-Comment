dd9a50216a7739859633556a5cb8acfea54518ee
FDMcreative <federico.delmonte@gmail.com>
2018-07-13 19:15:52 +0200

    register and login working

--- models/user.js
+++ models/user.js
@@ -1,6 +1,17 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
+const commentSchema = new mongoose.Schema({
+  content: {type:String, required:true},
+  createdBy: {type: mongoose.Schema.ObjectId, ref: 'User', required:true}
+}, {
+    timestamps: true
+});
+
+commentSchema.methods.ownedBy = function ownedBy(user) {
+  return this.createdBy.id === user.id;
+};
+
 const userSchema = new mongoose.Schema({
   username: { type:String, required:true },
   name: {type:String, required:true},
@@ -36,3 +47,22 @@ userSchema.methods.validatePassword = function validatePassword(password) {
 };
 
 module.exports = mongoose.model('User', userSchema);
+
+
+
+/////////////////////////////////////////////
+
+const hotelSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  address: {
+    line1: {type: String, required: true},
+    line2: {type: String},
+    city: {type: String, required:true},
+    postcode: {type: String, required:true},
+    country: {type: String, required:true}
+  },
+  image: {type: String, required:true},
+  stars: {type: Number, required:true},
+  createdBy: {type: mongoose.Schema.ObjectId, ref: 'User', required: true},
+  comments: [commentSchema]
+});
